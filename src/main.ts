72a688521b6e1e65f3fd01b63236d1b439db6c89
Aleksei Golubkov <nuriver@gmail.com>
2024-11-15 10:55:44 +0100

    fix: make env var available

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import * as YAML from 'yamljs';
+import { config } from 'dotenv';
+config();
 
 const PORT = process.env.PORT || 4000;
 
